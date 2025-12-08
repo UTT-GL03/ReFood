132cd5e501e5a9c5eb60e87bf6f1b2d9abe570ac
mohammadahamad <mo18ahamad@gmail.com>
2025-12-08 15:18:33 +0100

    test avec couchdb

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -5,18 +5,22 @@ import Offre from '../components/Offre'
 function Home() {
   const [offres, setOffres] = useState([])
 
-  useEffect(() => {
-    fetch('/sample_data.json')
-  .then(res => res.json())
-  .then(data => setOffres(data.docs.filter(X => X.type)))
-  .catch(err => console.error("Erreur chargement offres :", err))
-  }, [])
+useEffect(() => {
+  fetch('http://localhost:5984/refood_db/_all_docs?include_docs=true', {
+    headers: {
+      'Authorization': 'Basic ' + btoa('refooduser:refoodmdp')
+    }
+  })
+    .then(res => res.json())
+    .then(data => setOffres(data.rows.map(row => row.doc)))
+    .catch(err => console.error("Erreur chargement offres :", err))
+}, [])
+
 
   return (
     <main className="container">
       {offres.map((offre) => (
         <article key={offre._id} className="offre">
-          {/* Lien interne vers la page détail */}
           <a href={`/offre/${offre._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
             <Offre {...offre} />
           </a>
